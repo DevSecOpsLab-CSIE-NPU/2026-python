ea6c87ca48732ca5069409f1d6051b63d94ee663
1112405041 <1112405041@example.com>
2026-06-04 09:59:54 +0800

    test: add failing tests for UVA 11417 GCD

---
+++ weeks/week-15/solutions/1112405041/0603/test_gcd.py
@@ -0,0 +1,13 @@
+﻿import unittest
+try:
+    from gcd import sum_of_gcd
+except ImportError:
+    sum_of_gcd = None
+
+class TestSumOfGcd(unittest.TestCase):
+    def test_n_equals_2(self): self.assertEqual(sum_of_gcd(2), 1)
+    def test_n_equals_10(self): self.assertEqual(sum_of_gcd(10), 67)
+    def test_edge_case_n1(self): self.assertEqual(sum_of_gcd(1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
