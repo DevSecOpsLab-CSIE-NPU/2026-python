1457d54f6aab5211cc1e3c9991260bd7e086ff43
augchao <augchao@gms.npu.edu.tw>
2026-06-04 08:27:53 +0800

    feat: implement UVA 11417 GCD

--- weeks/week-15/solutions/fychao/0603/gcd.py
+++ weeks/week-15/solutions/fychao/0603/gcd.py
@@ -2,6 +2,7 @@ from math import gcd
 
 
 def sum_of_gcd(n: int) -> int:
+    """Return sum of gcd(i, j) for all 1 <= i < j <= n."""
     total = 0
     for i in range(1, n):
         for j in range(i + 1, n + 1):
