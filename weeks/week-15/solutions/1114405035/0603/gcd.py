4871f15869d9ec84e6ed8e3e356c170a00b9e048
LAI,YEN-TING <d14405035@ems.npu.edu.tw>
2026-06-04 08:37:34 +0800

    feat: implement UVA 11417 GCD

---
+++ weeks/week-15/solutions/1114405035/0603/gcd.py
@@ -0,0 +1,10 @@
+import math
+
+
+def sum_of_gcd(n: int) -> int:
+    """計算 1 <= i < j <= n 範圍內所有 gcd(i, j) 的總和。"""
+    g = 0
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            g += math.gcd(i, j)
+    return g
