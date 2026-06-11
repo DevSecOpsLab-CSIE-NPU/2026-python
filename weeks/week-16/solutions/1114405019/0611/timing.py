369b3a3d356f6b2766f4aeb7461f22d14911f6ab
Underiger <harryyiteng@gmail.com>
2026-06-11 09:46:02 +0800

    feat: stage1 實作 timeit 裝飾器

---
+++ weeks/week-16/solutions/1114405019/0611/timing.py
@@ -0,0 +1,17 @@
+import functools
+import time
+
+
+def timeit(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        elapsed = time.perf_counter() - start
+        wrapper.last_elapsed = elapsed
+        wrapper.records.append(elapsed)
+        return result
+
+    wrapper.last_elapsed = None
+    wrapper.records = []
+    return wrapper
