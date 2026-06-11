579b3aae5b30d3f4494ef6d784533f92eab60828
郭家瑋 <s11104225@school.saihs.edu.tw>
2026-06-11 09:07:18 +0800

    test: stage2 排序共用測試

---
+++ weeks/week-16/solutions/1114405028/0611/test_sorts.py
@@ -0,0 +1,46 @@
+import unittest
+import random
+
+import sorts
+
+
+class TestSorts(unittest.TestCase):
+    def setUp(self):
+        self.funcs = [
+            sorts.bubble_sort,
+            sorts.quick_sort,
+            sorts.merge_sort,
+        ]
+
+    def test_sort_correctness_and_no_modify(self):
+        data = [5, 3, 1, 4, 2]
+        expected = sorted(data)
+        for func in self.funcs:
+            with self.subTest(func=func.__name__):
+                arr = list(data)
+                result = func(arr)
+                self.assertEqual(result, expected)
+                self.assertEqual(arr, data)  # input not modified
+                self.assertIsNot(result, arr)  # returned new list
+
+    def test_edge_empty_list(self):
+        for func in self.funcs:
+            with self.subTest(func=func.__name__):
+                arr = []
+                res = func(arr)
+                self.assertEqual(res, [])
+                self.assertEqual(arr, [])
+
+    def test_repeats_and_negatives(self):
+        data = [3, -1, 2, 3, -5, 0]
+        expected = sorted(data)
+        for func in self.funcs:
+            with self.subTest(func=func.__name__):
+                arr = list(data)
+                res = func(arr)
+                self.assertEqual(res, expected)
+                self.assertEqual(arr, data)
+
+
+if __name__ == '__main__':
+    unittest.main()
