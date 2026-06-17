1c7d10018c9c91be63d10c959ebb9ab9e1da9e47
LUO HAO WEN <d14405048@ems.npu.edu.tw>
2026-06-17 14:09:22 +0800

    feat: 0617 實作 linear_search / binary_search

--- weeks/week-17/solutions/48/0617/search.py
+++ weeks/week-17/solutions/48/0617/search.py
@@ -1,6 +1,22 @@
 def linear_search(data, target):
-    return None
+    if data is None:
+        raise ValueError("data must not be None")
+    for i, val in enumerate(data):
+        if val == target:
+            return i
+    return -1
 
 
 def binary_search(data, target):
-    return None
+    if data is None:
+        raise ValueError("data must not be None")
+    left, right = 0, len(data) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if data[mid] == target:
+            return mid
+        elif data[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
