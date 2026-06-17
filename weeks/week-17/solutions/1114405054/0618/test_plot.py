892887a007afb2d3d78ffc5ffb792df151088d52
蔡珽州 <d14405054@ems.npu.edu.tw>
2026-06-18 00:02:46 +0800

    test: stage4 繪圖輸出測試

---
+++ weeks/week-17/solutions/1114405054/0618/test_plot.py
@@ -0,0 +1,12 @@
+import os
+import unittest
+
+
+class TestPlot(unittest.TestCase):
+
+    def test_radar_png_exists(self):
+        self.assertTrue(os.path.exists("assets/radar.png"))
+
+    def test_radar_png_not_empty(self):
+        size = os.path.getsize("assets/radar.png")
+        self.assertGreater(size, 1000)
