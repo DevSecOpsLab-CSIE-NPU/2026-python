6139f3ef33ac9260edb04c1021ad832dd6cb3e16
LUO HAO WEN <d14405048@ems.npu.edu.tw>
2026-06-17 14:06:33 +0800

    feat: 0617 實作 timeit 裝飾器

--- weeks/week-17/solutions/48/0617/timing.py
+++ weeks/week-17/solutions/48/0617/timing.py
@@ -1,8 +1,29 @@
+import functools
+import time
+
+
 def timeit(func=None, repeat=3):
     if func is None:
         return lambda f: timeit(f, repeat=repeat)
 
-    def wrapper(*args, **kwargs):
-        return None
+    if repeat < 1:
+        raise ValueError("repeat must be >= 1")
 
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        times = []
+        for _ in range(repeat):
+            t0 = time.perf_counter()
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                t1 = time.perf_counter()
+                times.append(t1 - t0)
+
+        wrapper.last_elapsed = sum(times) / repeat
+        wrapper.records = times
+        return result
+
+    wrapper.records = []
+    wrapper.last_elapsed = 0.0
     return wrapper
