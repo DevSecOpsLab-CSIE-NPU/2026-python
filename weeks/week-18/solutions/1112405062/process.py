08b02fc96d00262ca7e372f6720b4e0525f37105
1112405062 <1112405062@student.npu.edu.tw>
2026-06-22 18:57:30 +0800

    feat: implement process function

--- weeks/week-18/solutions/1112405062/process.py
+++ weeks/week-18/solutions/1112405062/process.py
@@ -1,2 +1,10 @@
 def process(nums: list[int], D: int = 4) -> list[int]:
-    return []
+    seen = set()
+    deduped = []
+    for x in nums:
+        if x not in seen:
+            deduped.append(x)
+            seen.add(x)
+    filtered = [x for x in deduped if x % D == 0]
+    filtered.sort()
+    return filtered
