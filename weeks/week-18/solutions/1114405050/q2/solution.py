5ab7079fa0cc3fe516e318810aeb942184d7c3fe
kai <d14405050@ems.npu.edu.tw>
2026-06-22 19:45:01 +0800

    feat: implement Caesar Cipher logic for Q2 TDD green light stage

--- weeks/week-18/solutions/1114405050/q2/solution.py
+++ weeks/week-18/solutions/1114405050/q2/solution.py
@@ -1,5 +1,21 @@
 def caesar_cipher(text, shift=2):
     """
     實作凱撒密碼邏輯
+    大寫：A-Z 內循環
+    小寫：a-z 內循環
+    其餘：保留
     """
-    pass
+    result = []
+    for char in text:
+        if 'a' <= char <= 'z':
+            # 小寫字母位移
+            new_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            result.append(new_char)
+        elif 'A' <= char <= 'Z':
+            # 大寫字母位移
+            new_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            result.append(new_char)
+        else:
+            # 非字母字元保留
+            result.append(char)
+    return "".join(result)
