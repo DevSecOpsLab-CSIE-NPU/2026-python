431de1e190e82154fd1ce4cbcdea961b60a158da
蔡珽州 <d14405054@ems.npu.edu.tw>
2026-06-22 18:58:33 +0800

    test: add Q3 test cases (get_digital_root)

---
+++ weeks/week-18/solutions/q3/test_q3.py
@@ -0,0 +1,24 @@
+import unittest
+from q3 import get_digital_root
+
+
+class TestDigitalRoot(unittest.TestCase):
+
+    def test_zero(self):
+        self.assertEqual(get_digital_root(0, 5), 0)
+
+    def test_eight(self):
+        self.assertEqual(get_digital_root(8, 5), 4)
+
+    def test_sixty_three(self):
+        self.assertEqual(get_digital_root(63, 5), 3)
+
+    def test_equal_to_base(self):
+        self.assertEqual(get_digital_root(5, 5), 1)
+
+    def test_single_digit(self):
+        self.assertEqual(get_digital_root(1, 5), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
